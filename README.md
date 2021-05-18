b8822da2baad4a50624ee381f79392568c1196ff
prakhar2109 <prakharagarwal21091998@gmail.com>
2021-05-18 18:58:03 +0530

    documentaion and code added

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# Starve-free-Reader-Writer-Soln
-##PROBLEM
+# PROBLEM
 
 The Reader-Writer problem  is an example of a synchronisation problem. There are two processes Reader and Writer that are sharing common resources e.g file at a time.
 The former one only reads the database, whereas the latter one wants to update (that is, to read and write) the database. 
@@ -8,7 +7,7 @@ The problem is when one process starts writing the database and another performs
 ###
 Another problem arises when one process starts writing the database and another process also starts writing. As both processes want to update the value of the data present in the file at the same time it will lead to the inconsistency in the database. 
 
-## Reader-Writer is categorized into three variants.
+### Reader-Writer is categorized into three variants.
 ### Give readers priority: 
 when there is at least one reader currently accessing the resource, allow new readers to access it as well. This can cause starvation if there are writers waiting to modify the resource and new readers arrive all the time, as the writers will never be granted access as long as there is at least one active reader.
 ### Give writers priority:
---
+++ solution.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
+
+int val = 0,readers_count = 0;
+sem_t seq_mutex, db_mutex, r_mutex;
+
+
+void *reader(void *id)
+{
+  
+  //Entry Section
+  sem_wait(&seq_mutex);  //Remeber the order of arrival (wait for turn)
+  sem_wait(&r_mutex);    //preventing more than one access to below segment
+  readers_count++;       
+ 
+  if(readers_count==1) sem_wait(&db_mutex);  // if it is the first reader then get the access to the reader for accessing the critical section and block the writer from accessing it
+ 
+  sem_post(&seq_mutex); 
+  sem_post(&r_mutex); 
+  printf("val read by the reader%d is %d\n",*((int *) id),val);
+
+  //EXIT SECTION
+  sem_wait(&r_mutex); 
+    readers_count--; 
+    if(readers_count==0) sem_post(&db_mutex); // if it is the last reader then release the access to the critical section so that it can be used by any waiting writer
+   
+  sem_post(&r_mutex); 
+  return id;
+}
+
+void *writer(void *id)
+{
+  sem_wait(&seq_mutex); //to maintain request order in fifo manner.
+  sem_wait(&db_mutex);  //get the access to the writer for accessing the critical section and block the readers from accessing it
+  sem_post(&seq_mutex); // release the semaphore to get the next reader/ writer to be serviced
+  val = val* 3 + 6;
+  printf("val written by the writer%d is %d\n",*((int *) id),val);
+  sem_post(&db_mutex);
+  return id;
+}
+
+int main(){
+
+  pthread_t readers[5],writers[5];
+ 
+  sem_init(&seq_mutex,0,1);
+  sem_init(&r_mutex,0,1);
+  sem_init(&db_mutex,0,1);
+  
+ 
+  int pt;
+  for(pt=0;pt<=4;pt++)
+  {
+    pthread_create(&writers[pt],NULL,writer,(void *)&pt);
+    pthread_create(&readers[pt],NULL,reader,(void *)&pt);
+    
+  }
+
+  for(pt=0;pt<=4;pt++)
+  {
+    pthread_join(writers[pt],NULL);
+    pthread_join(readers[pt],NULL);
+  }
+  return 0;
+}
